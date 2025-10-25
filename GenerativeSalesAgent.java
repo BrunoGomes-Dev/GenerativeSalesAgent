766e2a3a60be3ca37f70da816b51d93f231c6d51
Matheus de Souza Silva <matheus1882014@outlook.com>
2025-10-25 11:52:50 -0300

    Refactor imports in GenerativeSalesAgent.java

--- GenerativeSalesAgent.java
+++ GenerativeSalesAgent.java
@@ -1,7 +1,13 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
+
+   
+
 
 public class GenerativeSalesAgent {
     private static final String OPENAI_API_KEY = "SUA_API_KEY_AQUI";
@@ -65,3 +71,7 @@ public class GenerativeSalesAgent {
 
 
 }
+      
+        
+
+ 
